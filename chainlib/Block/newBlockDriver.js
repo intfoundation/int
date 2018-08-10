4ac914d35671d41d96d9d5234732098ca822aa9b
apyangfuheyaaaaa <517174331@qq.com>
2018-08-10 18:05:24 +0800

    block driver

--- chainlib/Block/newBlockDriver.js
+++ chainlib/Block/newBlockDriver.js
@@ -1,200 +1,132 @@
 "use strict";
 const EventEmitter = require('events');
+const assert = require('assert');
 
-class NewBlockDriver extends EventEmitter{
-    constructor(number,id,txStorage,devmode=false){
+class NewBlockDriver extends EventEmitter {
+    constructor(number, id, intervalTime, txStorage, devmode = false) {
         super();
-        this.nodeTurns=[];//记录参与出块的节点的id列表
-        this.currNode=-1; //当前出块的id
-        this.m_id=id;//自己的id
-        this.receivedIDs=[]; //新加入的节点“下一轮”出块，它来负责触发更新nodeTurns
-        this.bInTurns=false;
-        this.intervalTime = 5*1000;//出块时间间隔
-        this.moniterTimerID = 0;
-        this.newBlockTimer = 0;
+        this.nodeTurns = [];//记录参与出块的节点的id列表
+        this.prevNode = -2;
+        this.m_id = id;//自己的id
+        this.intervalTime = intervalTime;//出块时间间隔
         this.number = number;//分配的顺序number
         this.txStorage = txStorage;
-        this.devmode = devmode;
-
-        this.txStorage.on('OnNewTx',(storageObj) => {
-            if (this.devmode) {
-                if (this.currNode === this.m_id) {
-                    this._beginNewBlock(true);
-                }
-                else {
-                    if (this.moniterTimerID === 0) {
-                        this._beginMoniter();
-                    }
-                }
-            }
-            else{
-                if (this.currNode === this.m_id) {
-                    if (this.newBlockTimer === 0) {
-                        this._beginNewBlock();
-                    }
-                }
-                else {
-                    if (this.moniterTimerID === 0) {
-                        this._beginMoniter();
-                    }
-                }
-            }
-        });
-
-        this.txStorage.on('OnEmpty',(storageObj) => {
-            this._endMoniter();
-            this._endNewBlockTimer();
-        });
+        this.devmode = devmode?devmode:0;
+        this.begintimes = 1483200000000; //2017-01-01 00:00:00
+        this.lastblocktime = 0;//
+        this.monitorTimer = 0;
+        this.newBlockTimer = 0;
     }
 
-    //
-    updateTurns(turns){
-        if(this._compareList(turns,this.nodeTurns))
-        {
-            return ;
+    checkTurn(time, id) {
+        if (this.nodeTurns.length === 0) {
+            console.log(`[newBlockDriver checkTurn] nodeTruns.length=0`);
+            return true;
         }
-        this.receivedIDs=[];
-        this.nodeTurns = turns;
-        if(!this.bInTurns)
-        {
-            for(let i=0;i<this.nodeTurns.length;i++){
-                if(this.nodeTurns[i] === this.m_id)
-                {
-                    this._endMoniter();
-                    this.bInTurns = true;
-                    break;
-                }
-            } 
-            // if(this.moniterTimerID === 0)
-            // {
-            //     this._endMoniter()
-            //     this.moniterTimerID = setInterval(() => {
-            //         console.log("newBlockDriver,updateTurns,the long timer trigger,nodeid="+this.m_id.toString());
-            //         //长时间等待还收不到自己入队列信息，那可能出问题，自己出块
-            //         //这种情况理论上存在，实际上应该不用考虑，但是以防万一
-            //         this.bInTurns = true;
-            //         this._beginNewBlock();
-            //     },this.intervalTime*2*this.number);
-            // }
+        let n = this._getTimeSolt(time);
+        let nIndex = n % this.nodeTurns.length;
+        if (this.prevNode !== this.nodeTurns[nIndex] && id === this.nodeTurns[nIndex]) {
+            return true;
         }
+        console.log(`[newBlockDriver checkTurn] thisid=${this.m_id},prevNode=${this.prevNode},id=${id}, nIndexID=${this.nodeTurns[nIndex]}`);
+
+        return false;
     }
 
-    next(prevId){
-        if(!this.bInTurns)
-        {
+    updateTurns(turns) {
+        if (this._compareList(turns, this.nodeTurns)) {
             return;
         }
+        this.nodeTurns = turns;
+    }
 
-        let oldIndex=this.currNode;
-        let currIndex = this._getIndex(prevId);
-        currIndex++;
-        if(currIndex === this.nodeTurns.length)
-        {
-            currIndex = 0;
-        }
-        this.currNode = this.nodeTurns[currIndex];
-        /*
-        let id = this.nodeTurns[oldIndex];
-        let temp=[]
-        for (let i=0;i<this.receivedIDs.length;i++)
-        {
-            if(this.receivedIDs[i] === id)
-            {
-                this.receivedIDs.shift();
-                temp=[]
-                break;
+    next(prevId, blocktime) {
+        this.prevNode = prevId;
+        this.lastblocktime = blocktime;
+        this.beginNewBlock();
+    }
+
+    beginNewBlock() {
+        this._endNewBlockTimer();
+        this._endMonitorTimer();
+        let timeOffset = this.intervalTime;
+        if (this.lastblocktime > 0) {
+            let nLastSolt = this._getTimeSolt(this.lastblocktime);
+            let now = Date.now();
+            let n = this._getTimeSolt(now);
+            //assert(n >= nLastSolt, `beginNewBlock error n=${n},nLastSolt=${nLastSolt},now=${now},lastblocktime=${this.lastblocktime}`);
+            if (nLastSolt === n) {
+                timeOffset = n * this.intervalTime + this.begintimes + 100 - now; //离下一个solt的时间间隔
+            } else if (n > nLastSolt) {
+                timeOffset = 0;
+            } else {
+                console.log(`[newBlockDriver beginNewBlock] error n=${n},nLastSolt=${nLastSolt},now=${now},lastblocktime=${this.lastblocktime}`);
             }
-            else
-            {
-                temp.push(this.receivedIDs.shift());
-            }
+            console.log(`[newBlockDriver beginNewBlock] thisid=${this.m_id},lastsolt=${nLastSolt},nowsolt=${n},timeOffset=${timeOffset},min=${(n-1)*this.intervalTime+this.begintimes},now=${now}, max=${(n)*this.intervalTime+this.begintimes}`);
         }
-        //在receivedIDs中没有找到id，那么把temp给回receivedIDs，并把id压到最后面
-        if(temp.length !== 0)
-        {
-            this.receivedIDs=temp;
-            this.receivedIDs.push(id);
+
+        if (timeOffset === 0) {
+            this._newBlockImpl();
         }
-        if(this.receivedIDs.length === 0)
-        {
-            //更新出块序列，把新加上来的节点进入出块列表
-            this.emit("OnUpdateTurns",this);
+        this.newBlockTimer = setTimeout(() => {
+            this._newBlockImpl();
+        }, timeOffset);
+    }
+
+    _newBlockImpl() {
+        let now = Date.now();
+        let n = this._getTimeSolt(now);
+        let nIndex = n % this.nodeTurns.length;
+        if (nIndex === 0) {
+            this.emit("OnUpdateTurns", this);
         }
-        */
-       if(currIndex === 0)
-       {
-           //更新出块序列，把新加上来的节点进入出块列表
-           this.emit("OnUpdateTurns",this);
-       }
-        if(this.currNode === this.m_id)
-        {
-            //轮到自己出块了
-            this._beginNewBlock();    
+        this.currNode = this.nodeTurns[nIndex];
+        console.log(`[newBlockDriver _newBlockImpl] thisid=${this.m_id},trunid=${this.nodeTurns[nIndex]},min=${n*this.intervalTime+this.begintimes},now=${Date.now()}`);
+        if (this.nodeTurns[nIndex] === this.m_id) {
+            this.emit("OnNewBlock", this, this.nodeTurns, now);
         }
-        else
-        {
-            //重新开启监听器
-            this._beginMoniter();
+
+        this._beginMonitorTimer();
+    }
+
+    _getTimeSolt(time) {
+        let n = Math.floor((time - this.begintimes) / this.intervalTime);
+        if (n * this.intervalTime + this.begintimes < time) {
+            n = n + 1;
+        }
+
+        return n;
+    }
+
+    _beginMonitorTimer() {
+        this._endMonitorTimer();
+        this.monitorTimer = setTimeout(() => {
+            console.log(`[newBlockDriver _beginMonitorTimer]  thisid=${this.m_id}, intervalTime=${this.intervalTime}`);
+            this._newBlockImpl();
+        }, this.intervalTime);
+    }
+
+    _endMonitorTimer() {
+        if (this.monitorTimer !== 0) {
+            clearTimeout(this.monitorTimer);
+            this.monitorTimer = 0;
         }
     }
 
     _endNewBlockTimer() {
         if (this.newBlockTimer !== 0) {
-            clearInterval(this.newBlockTimer);
+            clearTimeout(this.newBlockTimer);
             this.newBlockTimer = 0;
         }
     }
 
-    _beginNewBlock(bNow=false){
-        this._endMoniter();
-        this._endNewBlockTimer();
-        if (this.txStorage.getCount() ===0) {
-            return;
-        }
-        if (bNow) {
-            this.emit("OnNewBlock",this,this.nodeTurns);
-        }
-        else {
-            this.newBlockTimer = setInterval(() => {
-                if (this.newBlockTimer !== 0) {
-                    this._endNewBlockTimer();
-                }
-                this.emit("OnNewBlock",this,this.nodeTurns);
-            },this.intervalTime);
-        }
-    }
-
-    _endMoniter(){
-        if(this.moniterTimerID !== 0)
-        {
-            clearInterval(this.moniterTimerID);
-            this.moniterTimerID = 0;
-        }
-    }
-    _beginMoniter(){
-        this._endMoniter()
-        if (this.txStorage.getCount() ===0) {
-            return;
-        }
-        this.moniterTimerID = setInterval(() => {
-            console.log("newBlockDriver.js _beginMoniter trigger");
-            if (this.moniterTimerID !== 0) {
-                this._endMoniter();
-            }
-            this.next(this.currNode);
-        },this.intervalTime*3/2);
-    }
-
-    _compareList(list1,list2){
-        if(list1.length !== list2.length)
-        {
+    _compareList(list1, list2) {
+        if (list1.length !== list2.length) {
             return false;
         }
 
-        for(let i=0;i<list1.length;i++)
-        {
-            if(list1[i] != list2[i])
-            {
+        for (let i = 0; i < list1.length; i++) {
+            if (list1[i] != list2[i]) {
                 return false;
             }
         }
@@ -202,11 +134,9 @@ class NewBlockDriver extends EventEmitter{
         return true;
     }
 
-    _getIndex(nodeID){
-        for(let i=0;i<this.nodeTurns.length;i++)
-        {
-            if(nodeID===this.nodeTurns[i])
-            {
+    _getIndex(nodeID) {
+        for (let i = 0; i < this.nodeTurns.length; i++) {
+            if (nodeID === this.nodeTurns[i]) {
                 return i;
             }
         }
