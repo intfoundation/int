b40891a2c97f02783be0aae8bbf41cc1fe4c469c
ali <zhangali815432952@163.com>
2018-11-01 14:22:50 +0800

    modify a parameter

--- src/tool/startMiner.js
+++ src/tool/startMiner.js
@@ -51,7 +51,7 @@ async function run(argv) {
     options.set("bdt_log_level", "info");
     options.set("saveMismatch", true);
     options.set("dataDir", './data/intchain/minerData');
-    options.set("sn", "SN_PEER_TEST1@testsn.intchain.io@8550@8551");
+    options.set("sn", "SN_PEER_TEST@testsn.intchain.io@8550@8551");
     let exit = false;
     exit = !(await client_1.host.initMiner(command.options)).ret;
     if (exit) {
