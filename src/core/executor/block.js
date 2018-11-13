c90d1e7d173917a20c8759b86a3d79fc3c0f89ed
ali <zhangali815432952@163.com>
2018-11-13 20:31:48 +0800

    fix a bug about dpos

--- src/core/executor/block.js
+++ src/core/executor/block.js
@@ -1,161 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const assert = require("assert");
 const error_code_1 = require("../error_code");
-const transaction_1 = require("./transaction");
-class BlockExecutor {
-    constructor(options) {
-        this.m_storage = options.storage;
-        this.m_handler = options.handler;
-        this.m_block = options.block;
-        this.m_externContext = options.externContext;
-        this.m_logger = options.logger;
-        Object.defineProperty(this.m_externContext, 'logger', {
-            writable: false,
-            value: this.m_logger
-        });
-        this.m_globalOptions = options.globalOptions;
-    }
-    get externContext() {
-        return this.m_externContext;
-    }
-    _newTransactionExecutor(l, tx) {
-        return new transaction_1.TransactionExecutor(l, tx, this.m_logger);
-    }
-    _newEventExecutor(l) {
-        return new transaction_1.EventExecutor(l, this.m_logger);
-    }
-    async execute() {
-        let t1 = Date.now();
-        let ret = await this._execute(this.m_block);
-        let t2 = Date.now();
-        this.m_logger.info(`runblock time====${t2 - t1}, count=${this.m_block.content.transactions.length}`);
-        return ret;
-    }
-    async verify() {
-        let oldBlock = this.m_block;
-        this.m_block = this.m_block.clone();
-        let err = await this.execute();
-        if (err) {
-            if (err === error_code_1.ErrorCode.RESULT_TX_CHECKER_ERROR) {
-                return { err: error_code_1.ErrorCode.RESULT_OK, valid: error_code_1.ErrorCode.RESULT_TX_CHECKER_ERROR };
-            }
-            else {
-                return { err };
-            }
-        }
-        if (this.m_block.hash !== oldBlock.hash) {
-            this.m_logger.error(`block ${oldBlock.number} hash mismatch!! 
-            except storage hash ${oldBlock.header.storageHash}, actual ${this.m_block.header.storageHash}
-            except hash ${oldBlock.hash}, actual ${this.m_block.hash}
-            `);
-        }
-        if (this.m_block.hash === oldBlock.hash) {
-            return { err: error_code_1.ErrorCode.RESULT_OK, valid: error_code_1.ErrorCode.RESULT_OK };
-        }
-        else {
-            return { err: error_code_1.ErrorCode.RESULT_OK, valid: error_code_1.ErrorCode.RESULT_VERIFY_NOT_MATCH };
-        }
-    }
-    async _execute(block) {
-        this.m_logger.info(`begin execute block ${block.number}`);
-        let err = await this.executePreBlockEvent();
-        if (err) {
-            this.m_logger.error(`blockexecutor execute begin_event failed,errcode=${err},blockhash=${block.hash}`);
-            return err;
-        }
-        let ret = await this._executeTransactions();
-        if (ret.err) {
-            this.m_logger.error(`blockexecutor execute method failed,errcode=${ret.err},blockhash=${block.hash}`);
-            return ret.err;
-        }
-        err = await this.executePostBlockEvent();
-        if (err) {
-            this.m_logger.error(`blockexecutor execute end_event failed,errcode=${err},blockhash=${block.hash}`);
-            return err;
-        }
-        let receipts = ret.value;
-        // 票据
-        block.content.setReceipts(receipts);
-        // 更新块信息
-        return await this._updateBlock(block);
-    }
-    async executeBlockEvent(listener) {
-        let exec = this._newEventExecutor(listener);
-        let ret = await exec.execute(this.m_block.header, this.m_storage, this.m_externContext);
-        if (ret.err || ret.returnCode) {
-            this.m_logger.error(`block event execute failed`);
-            return error_code_1.ErrorCode.RESULT_EXCEPTION;
-        }
-        return error_code_1.ErrorCode.RESULT_OK;
-    }
-    async executePreBlockEvent() {
-        if (this.m_block.number === 0) {
-            // call initialize
-            if (this.m_handler.genesisListener) {
-                const err = await this.executeBlockEvent(this.m_handler.genesisListener);
-                if (err) {
-                    this.m_logger.error(`handler's genesisListener execute failed`);
-                    return error_code_1.ErrorCode.RESULT_EXCEPTION;
-                }
-            }
-        }
-        let listeners = await this.m_handler.getPreBlockListeners(this.m_block.number);
-        for (let l of listeners) {
-            const err = await this.executeBlockEvent(l);
-            if (err) {
-                return err;
-            }
-        }
-        return error_code_1.ErrorCode.RESULT_OK;
-    }
+const value_chain_1 = require("../value_chain");
+const consensus = require("./consensus");
+class DposBlockExecutor extends value_chain_1.ValueBlockExecutor {
     async executePostBlockEvent() {
-        let listeners = await this.m_handler.getPostBlockListeners(this.m_block.number);
-        for (let l of listeners) {
-            const err = await this.executeBlockEvent(l);
-            if (err) {
-                return err;
+        let err = await super.executePostBlockEvent();
+        if (err) {
+            return err;
+        }
+        if (this.m_block.number > 0) {
+            let dbr = await this.m_storage.getReadWritableDatabase(value_chain_1.Chain.dbSystem);
+            if (dbr.err) {
+                this.m_logger.error(`execute block failed for get system database ${dbr.err}`);
+                return dbr.err;
+            }
+            let denv = new consensus.Context(dbr.value, this.m_globalOptions, this.m_logger);
+            // 修改miner的最后一次出块时间
+            // 创世快不算时间，因为创世快产生后可能很长时间才开始出其他块的
+            await denv.updateProducerTime(this.m_block.header.miner, this.m_block.header.timestamp);
+            // 维护被禁用miner信息
+            if (this.m_block.number % this.m_globalOptions.unbanBlocks === 0) {
+                await denv.unbanProducer(this.m_block.header.timestamp);
+            }
+            let bReSelect = false;
+            if (this.m_block.number % this.m_globalOptions.reSelectionBlocks === 0) {
+                // 先禁用那些超过最长时间不出块的miner
+                await denv.banProducer(this.m_block.header.timestamp);
+                // 更新选举结果
+                let ber = await denv.finishElection(this.m_block.header.timestamp.toString());
+                if (ber.err) {
+                    return ber.err;
+                }
+                bReSelect = true;
+            }
+            if (this.m_block.number === 1 || bReSelect) {
+                // 维护miner时间信息
+                await denv.maintain_producer(this.m_block.header.timestamp);
             }
         }
-        return error_code_1.ErrorCode.RESULT_OK;
-    }
-    async _executeTransactions() {
-        let receipts = [];
-        // 执行tx
-        for (let tx of this.m_block.content.transactions) {
-            const ret = await this.executeTransaction(tx);
-            if (ret.err) {
-                return { err: ret.err };
-            }
-            receipts.push(ret.receipt);
-        }
-        return { err: error_code_1.ErrorCode.RESULT_OK, value: receipts };
-    }
-    async executeTransaction(tx, flag) {
-        const checker = this.m_handler.getTxPendingChecker(tx.method);
-        if (!checker || checker(tx)) {
-            this.m_logger.error(`verfiy block failed for tx ${tx.hash} ${tx.method} checker failed`);
-            return { err: error_code_1.ErrorCode.RESULT_TX_CHECKER_ERROR };
-        }
-        let listener = this.m_handler.getTxListener(tx.method);
-        assert(listener, `no listener for ${tx.method}`);
-        if (!listener) {
-            return { err: error_code_1.ErrorCode.RESULT_NOT_SUPPORT };
-        }
-        let exec = this._newTransactionExecutor(listener, tx);
-        let ret = await exec.execute(this.m_block.header, this.m_storage, this.m_externContext, flag);
-        return ret;
-    }
-    async _updateBlock(block) {
-        // 写回数据库签名
-        const mdr = await this.m_storage.messageDigest();
-        if (mdr.err) {
-            return mdr.err;
-        }
-        block.header.storageHash = mdr.value;
-        block.header.updateContent(block.content);
-        block.header.updateHash();
         return error_code_1.ErrorCode.RESULT_OK;
     }
 }
-exports.BlockExecutor = BlockExecutor;
+exports.DposBlockExecutor = DposBlockExecutor;
