23fee150d1cb1a83acda0ff95e35ada341d4ec5f
ali <zhangali815432952@163.com>
2018-11-15 15:21:57 +0800

    modify an bug about dpos and log

--- src/core/dpos_chain/chain.js
+++ src/core/dpos_chain/chain.js
@@ -20,9 +20,6 @@ class DposChain extends value_chain_1.ValueChain {
     get _broadcastDepth() {
         return 0;
     }
-    get _ignoreVerify() {
-        return true;
-    }
     async initComponents(options) {
         let err = await super.initComponents(options);
         if (err) {
