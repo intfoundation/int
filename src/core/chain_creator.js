731484d5348a4d4af81f75038091a6c2fbd3f9f0
like <likeaixi@gmail.com>
2018-12-06 12:35:39 +0800

    Updated npm version and handler location

--- src/core/chain_creator.js
+++ src/core/chain_creator.js
@@ -85,7 +85,7 @@ class ChainCreator {
         // 如果是命令行启动，则替换掉 handler.js 的加载路径
         let dirPath = __dirname;
         if (dirPath.indexOf('node_modules') !== -1) {
-            handlerPath = path.join(__dirname, '../../', handlerPath);
+            handlerPath = path.join(__dirname, '../../', constConfig['handler']);
         }
         let typeOptions = constConfig['type'];
         if (!typeOptions || !typeOptions.consensus || !typeOptions.features) {
